d1054420b838c00b5e7690cf36875c73cc4cf8b8
msik314 <msik314@gmail.com>
2017-09-15 22:40:44 -0400

    Title theme plus sound playing

--- GameDev1/source/PlayState.hx
+++ GameDev1/source/PlayState.hx
@@ -1,12 +1,18 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxG;
+import flixel.system.FlxSound;
 
 class PlayState extends FlxState
 {
+    var _music:FlxSound;
 	override public function create():Void
 	{
 		super.create();
+        FlxG.sound.playMusic(AssetPaths.titleTheme__ogg, 1, false);
+        _music = FlxG.sound.music;
+        trace(_music.length);
 	}
 
 	override public function update(elapsed:Float):Void
