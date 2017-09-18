a86e3d31e599b0f6f09d87c0faa43fbc43ac34b1
figgyfunk <figa@rpi.edu>
2017-09-17 20:54:44 -0400

    getting rid of trial tilemaps and first test of loading ogmo maps in

--- GameDev1/source/PlayState.hx
+++ GameDev1/source/PlayState.hx
@@ -1,11 +1,20 @@
 package;
 
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.tile.FlxTilemap;
+import flixel.FlxObject;
 
 class PlayState extends FlxState
 {
+
+	private var _map:FlxOgmoLoader;
+	private var _mWalls:FlxTilemap;
 	override public function create():Void
 	{
+		_map = new FlxOgmoLoader(AssetPaths.level1_tight__oel);
+		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "wall");
+		add(_mWalls);
 		super.create();
 	}
 
