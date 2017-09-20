eab64d2d36fcc3a7e07f57a5d8d69028c7dfb990
Brandon Ip <Chupalika@gmail.com>
2017-09-20 15:34:50 -0400

    experimenting with cameras

--- GameDev1/source/PlayState.hx
+++ GameDev1/source/PlayState.hx
@@ -1,22 +1,30 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxObject;
 import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.tile.FlxTilemap;
 import flixel.group.FlxGroup;
 import flixel.text.FlxText;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState {
-	var _player:Player;
+	var player:Player;
 	var debugtext:FlxText;
 	
 	private var _level:FlxOgmoLoader;
 	private var _tilemap:FlxTilemap;
 	private var _wallGroup:FlxTypedGroup<Wall>;
 	
+	private var playercam:FlxCamera;
+	
 	override public function create():Void {
 		super.create();
+		
+		FlxG.resizeWindow(1024, 768);
+		FlxG.camera.bgColor = 0xFF555555;
 		
 		//copy this with correct file names to load levels
 		_level = new FlxOgmoLoader(AssetPaths.level__oel);
@@ -27,16 +35,21 @@ class PlayState extends FlxState {
 		_level.loadEntities(placeEntities, "entities");
 		add(_tilemap);
 		
-		_player = new Player(50, 50, _tilemap);
-		add(_player);
+		player = new Player(50, 50, _tilemap);
+		add(player);
 		
 		debugtext = new FlxText(10, 10, 300, "");
 		add(debugtext);
+		
+		playercam = new FlxCamera(Std.int(FlxG.width/2) - 100, Std.int(FlxG.height/2) - 100, 200, 200);
+		playercam.bgColor = 0xFFAAAAAA;
+		playercam.follow(player, FlxCameraFollowStyle.TOPDOWN);
+		FlxG.cameras.add(playercam);
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
-		debugtext.text = Std.string(_player._rot);
+		debugtext.text = Std.string(player._rot);
 	}
 	
 	private function placeEntities(entityName:String, entityData:Xml):Void {
